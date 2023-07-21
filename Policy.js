b6dafe0f833ee2063a808647b85e360c7fedf6bd
Nisha1668 <140076253+Nisha1668@users.noreply.github.com>
2023-07-21 12:48:10 +0530

    Add files via upload

---
+++ Policy.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Policy = () => {
+  return (
+    <Layout tite="Privacy Policy">
+        <div className='row contactus'>
+          <div className='col-md-6'>
+          <img
+          src="/images/contactus.jpeg"
+          alt="contactus"
+          style={{width:"100%",paddingTop:"40px"}} 
+          />
+          </div>
+          <div className='col-md-4'>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          </div>
+        </div>
+    </Layout>
+  )
+}
+
+export default Policy
